9cbfa574ddaac413d534d1ef6b8bda385c9c584e
zachMitchellAI <69049600+zachMitchellAI@users.noreply.github.com>
2026-09-21 11:00:47 -0400

    Apply troubleshooting note

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+// The base path should be wherever the dist should be outputted to on github pages.
+// If it's a standalone site without a domain, it should be the name of the repo
+// However, in our case, we have a custom domain, so it's just the root! https://github.com/vitejs/vite/discussions/13910
 export default defineConfig({
   plugins: [react()],
   base: "/",
